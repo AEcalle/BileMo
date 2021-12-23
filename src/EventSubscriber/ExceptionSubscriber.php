f3897ac5ae3b58eb26666c0f23c4471fd3c6bb1c
AEcalle <contact@aurelienecalle.fr>
2021-12-23 15:45:32 +0100

    Install/Config Lexik_JWT bundle

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace App\EventSubscriber;
 
+use Exception;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
