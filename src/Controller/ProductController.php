edede1f4eb27290bee772c7c1e5669f54e0174c0
AEcalle <contact@aurelienecalle.fr>
2022-01-11 16:49:53 +0100

    Improve doc

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,6 +8,7 @@ use App\Entity\Product;
 use App\Repository\ProductRepository;
 use App\Service\ItemsListFactory;
 use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security as NelmioSecurity;
 use OpenApi\Annotations as OA;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -19,21 +20,59 @@ use Symfony\Component\Serializer\SerializerInterface;
 final class ProductController
 {
     #[Route(name:'api_products_collection_get', methods:['GET'])]
-    /**
+/**
      * @OA\Get(summary="Get list of a products")
      * @OA\Response(
      *     response=200,
-     *     description="Return list of products",
+     *     description="Return products list",
      *     @OA\JsonContent(
-     *        type="array",
-     *        @OA\Items(ref=@Model(type=Product::class))
+     *        @OA\Property(property="page", description="Current page",  type="integer", example=1),
+     *        @OA\Property(property="pages" ,description="number total of pages", type="integer", example=10),
+     *        @OA\Property(property="limit",description="Products per page", type="integer", example=1),
+     *        @OA\Property(property="_links",
+     *               @OA\Property(
+     *                   property="href", 
+     *                   @OA\Property(property="first", type="string", example="https://127.0.0.1:8000/api/products?page=1"),
+     *                   @OA\Property(property="next", type="string", example="https://127.0.0.1:8000/api/products?page=2"),
+     *                   @OA\Property(property="previous", type="string", example="https://127.0.0.1:8000/api/products?page=1"),
+     *                   @OA\Property(property="last", type="string", example="https://127.0.0.1:8000/api/products?page=10"),
+     *                )
+     *          ),
+     *      @OA\Property(property="_embedded",
+     *      @OA\Property(property="items",type="array",
+     *        @OA\Items(
+     *          @OA\Property(property="id", type="integer", example=1),
+     *          @OA\Property(property="name", type="string", example="name0"),
+     *          @OA\Property(property="description", type="string", example="description0"),
+     *          @OA\Property(property="price", type="integer", example=335),
+     *          @OA\Property(property="tva", type="integer", example=20),
+     *          @OA\Property(property="color", type="string", example="color0"),
+     *          @OA\Property(property="brand", type="string", example="brand0"),
+     *          @OA\Property(property="os", type="string", example="os0"),
+     *          @OA\Property(property="memory", type="integer", example=8373),
+     *          @OA\Property(property="stock", type="integer", example=340),
+     *          @OA\Property(property="updatedAt", type="string", format="date-time"),
+     *          @OA\Property(property="_links", 
+     *              @OA\Property(property="self", type="string", example="https://127.0.0.1:8000/api/products/1"),
+     *          ),
+     *       ),
      *     )
+     *   )
+     *  )
      * )
      * @OA\Response(
      *     response=401,
      *     description="JWT Token not found",    
      * )
+     * @OA\Parameter(
+     *     name="page",
+     *     in="query",
+     *     description="Numéro de page",
+     *     @OA\Schema(type="int")
+     * )
      * @OA\Tag(name="Products")
+     * 
+     * @NelmioSecurity(name="Bearer")
      */
     public function collection (
         ProductRepository $productRepository, 
@@ -71,7 +110,13 @@ final class ProductController
      *     description="Return a product",
      *     @Model(type=Product::class)
      * )
+     * @OA\Response(
+     *    response=401,
+     *    description="JWT Token not found",
+     * )
      * @OA\Tag(name="Products")
+     * 
+     * @NelmioSecurity(name="Bearer")
      */
     public function item (
         Product $product, 
